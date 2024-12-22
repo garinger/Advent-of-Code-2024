274500c8b0bc48b6d76c9eb7f5437d52734e8ce1
Jackson Garinger <jackson.garinger@gmail.com>
2024-12-21 19:08:58 -0600

    Day 2 part 2

--- Day02/Solution.cpp
+++ Day02/Solution.cpp
@@ -45,8 +45,46 @@ int part_1() {
     return num_of_safe_reports;
 }
 
+bool is_safe(const std::vector<int>& levels) {
+    for (int i = 0; i < levels.size(); i++) {
+        bool increasing = levels[1] - levels[0] > 0;
+        for (int i = 0; i < levels.size() - 1; i++) {
+            int diff = levels[i + 1] - levels[i];
+            int delta = abs(diff);
+            if (increasing && diff < 0
+            || !increasing && diff >= 0
+            || delta < 1 || delta > 3) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 int part_2() {
-    return 0;
+    std::ifstream filein("Input.txt");
+    std::vector<std::vector<int>> reports;
+
+    for (std::string line; std::getline(filein, line);) {
+        reports.emplace_back(split_string_on_spaces(line));
+    }
+
+    int num_safe_reports = 0;
+    for (const auto& report : reports) {
+        for (int i = 0; i < report.size(); i++) {
+            std::vector<int> sub_report;
+            for (int j = 0; j < report.size(); j++) {
+                if (j == i) continue;
+                sub_report.push_back(report[j]);
+            }
+            if (is_safe(sub_report)) {
+                num_safe_reports++;
+                break;
+            }
+            sub_report.clear();
+        }
+    }
+    return num_safe_reports;
 }
 
 int main(int argc, char** argv) {
